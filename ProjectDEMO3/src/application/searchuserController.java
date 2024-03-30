64e39cd4ec6d271b3d0fa13d0d425ade8cf0509d
Karanveer Singh <ks393363@gmail.com>
2024-03-29 18:49:36 -0700

    Searchuser page fully functional.

--- ProjectDEMO3/src/application/searchuserController.java
+++ ProjectDEMO3/src/application/searchuserController.java
@@ -3,9 +3,11 @@ package application;
 import java.io.IOException;
 import java.net.URL;
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.time.LocalDate;
 import java.util.ResourceBundle;
 
 import javafx.collections.FXCollections;
@@ -18,6 +20,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
@@ -32,12 +35,33 @@ public class searchuserController implements Initializable{
 	@FXML public TableColumn<User, String> lastname;
 	@FXML public TableColumn<User, Integer> userid;
 	@FXML public TableColumn<User, String> username;
+	@FXML Label Firstnamelbl;
+	@FXML Label Lastnamelbl;
+	@FXML Label Usernamelbl;
+	@FXML Label Useridlbl;
+	@FXML Label Activebooklbl;
+	@FXML Label Bookidlbl;
+	@FXML Label Issuedatelbl;
+	@FXML Label bookidlabel;
+	@FXML Label issuedatelabel;
+	@FXML Label label1;
+	@FXML Label label2;
+	
+	
 	ObservableList<User> observablelist= FXCollections.observableArrayList();
 	
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		// TODO Auto-generated method stub
 		tableinit();
+		
+		
+		tableview.getSelectionModel().selectedItemProperty().addListener((obs, oldSelection, newSelection) -> {
+			if (newSelection != null) {
+				
+				displayDataInLabels();
+			}
+		});
 		
 		Homebtn.setOnAction(new EventHandler<ActionEvent>() {
 
@@ -69,13 +93,12 @@ public class searchuserController implements Initializable{
 		
 		
 		String query = "SELECT * FROM useraccounts;";
-		String querybook= "SELECT * FROM bookinfo WHERE BookId = ? ;"; 
 		
 		DatabaseConnection connectNow = new DatabaseConnection();
 		Connection connectDB = connectNow.getConnection();
 		
-		int userID, bookID;
-		String bookname, UserName;
+		int userID ;
+		String  UserName;
 		String FirstName, LastName;
 		
 		
@@ -91,7 +114,7 @@ public class searchuserController implements Initializable{
 				FirstName=queryResult.getString(2);
 				LastName=queryResult.getString(3);
 				UserName=queryResult.getString(4);
-				bookID=queryResult.getInt(7);
+				
 				
 				observablelist.add(new User(userID, FirstName, LastName, UserName));
 			}
@@ -104,5 +127,71 @@ public class searchuserController implements Initializable{
 		
 		tableview.setItems(observablelist);
 	}
-
+	
+	 private void displayDataInLabels() {
+	        // Get the selected user from the tableview
+	        User selectedUser = tableview.getSelectionModel().getSelectedItem();
+	        
+	        if (selectedUser != null) {
+	        	int userid = selectedUser.getUserid();
+	        	
+	        	
+	        	String query = "SELECT * FROM useraccounts WHERE iduserAccount = '"+userid+"';";
+	        	
+	        	DatabaseConnection connectNow = new DatabaseConnection();
+	        	Connection connectDB = connectNow.getConnection();
+	        	
+	        	try {
+					Statement statement=connectDB.createStatement();
+					ResultSet queryResult = statement.executeQuery(query);					
+					
+					queryResult.next();
+					
+					int bookidissued=queryResult.getInt(7);
+					if(bookidissued !=0)
+					{
+						String querybook= "SELECT * FROM bookinfo WHERE BookId = "+bookidissued+";";
+						Statement statement1=connectDB.createStatement();
+						ResultSet queryResultbook= statement1.executeQuery(querybook);
+						queryResultbook.next();
+						Activebooklbl.setText(queryResultbook.getString(2));
+						bookidlabel.setVisible(true);
+						Bookidlbl.setText(Integer.toString(bookidissued));
+						issuedatelabel.setVisible(true);
+						String issuedate = (queryResult.getDate(8)).toString();
+						Issuedatelbl.setText(issuedate);
+						Firstnamelbl.setText(selectedUser.getFirstname());
+				        Lastnamelbl.setText(selectedUser.getLastname());
+				        Usernamelbl.setText(selectedUser.getUsername());	  
+				        Useridlbl.setText(Integer.toString(selectedUser.getUserid()));
+				        label1.setVisible(true);
+				        label2.setVisible(true);
+				        queryResultbook.close();
+				        statement1.close();	       
+						
+					}
+					else {
+						Activebooklbl.setText("None");
+						bookidlabel.setVisible(false);
+						issuedatelabel.setVisible(false);
+						Bookidlbl.setText("");
+						Issuedatelbl.setText("");
+						label1.setVisible(false);
+						label2.setVisible(false);
+						
+						Useridlbl.setText(Integer.toString(selectedUser.getUserid()));
+						Firstnamelbl.setText(selectedUser.getFirstname());
+				        Lastnamelbl.setText(selectedUser.getLastname());
+				        Usernamelbl.setText(selectedUser.getUsername());	
+					}
+					
+					
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+	        	
+	        }
+	        
+	 }
 }
